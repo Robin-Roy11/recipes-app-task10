1704094e856715735ee3c06b4eca8825718656f3
Robin-Roy11 <robinroy.ax@gmail.com>
2025-05-07 22:25:28 +0530

    changes

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const app=express();
 // middleware to parse the request body
 app.use(express.json());
 app.use("/api/recipes",recipeRouter);
-
+app.get("/",(req,res)=>{
+    res.send("server is running..")
+})
 // middleware to handle 404 errors
 app.use(errorRoute);
 module.exports=app;
+
